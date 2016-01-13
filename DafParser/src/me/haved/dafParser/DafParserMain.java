e232ef5dac16ad6fee372e08326111c2e8e22de6
haved <krogstie.havard@gmail.com>
2016-01-13 23:22:33 +0100

    Start help text. Change .gitignore

--- DafParser/src/me/haved/dafParser/DafParserMain.java
+++ DafParser/src/me/haved/dafParser/DafParserMain.java
@@ -3,5 +3,15 @@ package me.haved.dafParser;
 public class DafParserMain {
 	public static void main(String[] args) {
 		System.out.println("The daf parser!");
+		if(args.length==0) {
+			System.out.println("You need to pass arguments. -h for help");
+			return;
+		}
+		if(args[0].equals("-h") | args[0].equals("--help") | args[0].equals("?")) {
+			System.out.println("Help file for the daf parser.\n"
+					+ "Turns .daf files into .h and .cpp files\n"
+					+ "daf inputDafFile\n"
+					+ "daf inputDafFile outputDir");
+		}
 	}
 }
