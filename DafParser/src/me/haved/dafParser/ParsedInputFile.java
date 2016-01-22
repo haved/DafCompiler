0fa6fe8bbde83dfe2e1c43645ca8eef051cfef5d
haved <krogstie.havard@gmail.com>
2016-01-22 23:02:26 +0100

    Use RootNode in ParsedInputFile as well

--- DafParser/src/me/haved/dafParser/ParsedInputFile.java
+++ DafParser/src/me/haved/dafParser/ParsedInputFile.java
@@ -2,12 +2,15 @@ package me.haved.dafParser;
 
 import java.io.File;
 
+import me.haved.dafParsed.node.RootNode;
+
 import static me.haved.dafParser.LogHelper.*;
 
 public class ParsedInputFile {
 	private File inputFile;
 	private String infileName;
-	private boolean parsed;
+	private RootNode root;
+	
 	public ParsedInputFile(File inputFile, String infileName) {
 		this.inputFile = inputFile;
 		this.infileName = infileName;
@@ -15,11 +18,11 @@ public class ParsedInputFile {
 	
 	public void parse() {
 		logAssert(inputFile.isFile(), "ParsedInputFile got a file that doesn't exist! Should never happen!");
-		parsed = true;
+		root = new RootNode();
 	}
 	
 	public void writeToCppAndHeader(File cppFile, File headerFile) {
-		logAssert(parsed, "Trying to write a non parsed daf file to .cpp and .h");
+		logAssert(root != null, "Trying to write a non parsed daf file to .cpp and .h");
 		log(infileName, INFO, "ParsedInputFile writing to files '%s' and '%s'", cppFile.getName(), headerFile.getName());
 	}
 }
