d319c05c3a0172d8a231f820f2248bbc19d9e888
haved <krogstie.havard@gmail.com>
2016-02-27 23:43:20 +0100

    Add #import and #using to DefinitionFinder

--- DafParser/src/me/haved/dafParser/semantic/DefinitionFinder.java
+++ DafParser/src/me/haved/dafParser/semantic/DefinitionFinder.java
@@ -9,10 +9,20 @@ public class DefinitionFinder {
 	public static Definition getDefinition(TokenDigger digger) {
 		boolean pub = false;
 		Token first = digger.currentAndAdvance();
-		if(first.getType()==TokenType.PUB) {
+		if(first==null)
+			return null;
+		TokenType firstType = first.getType();
+		if(firstType==TokenType.PUB) {
 			pub = true;
+		} else if(firstType==TokenType.DAF_IMPORT) {
+			return new IncludeDefinition(first.getLocation(), first.getText(), false);
+		} else if(firstType==TokenType.DAF_USING) {
+			return new IncludeDefinition(first.getLocation(), first.getText(), true);
 		}
 		first = digger.currentAndAdvance();
+		if(first == null)
+			return null;
+		firstType = first.getType();
 		if(pub)
 			log(first.getLocation().getErrorString(), DEBUG, "Found a public definition");
 		return null;
