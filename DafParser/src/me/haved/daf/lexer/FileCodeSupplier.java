905664f3729556dd58071f39f4a7c1d72186cac7
haved <krogstie.havard@gmail.com>
2016-03-26 20:34:58 +0100

    Start rewriting FileCodeSupplier to make it only arraycopy when needed

--- DafParser/src/me/haved/daf/lexer/FileCodeSupplier.java
+++ DafParser/src/me/haved/daf/lexer/FileCodeSupplier.java
@@ -7,9 +7,10 @@ import static me.haved.daf.LogHelper.*;
 public class FileCodeSupplier implements Supplier {
 	
 	private static int DEFAULT_CHAR_BUFFER_LENGTH = 30; // A macro will quickly destroy this
-	private static float ARRAY_EXPAND_AMOUNT = 1.5f;
+	private static float ARRAY_EXPAND_FACTOR = 1.5f;
 	
 	private FileTextSupplier fileText;
+	private MacroMap macros;
 	private char[] chars;
 	private int[] lineNums;
 	private int[] colNums;
@@ -18,22 +19,67 @@ public class FileCodeSupplier implements Supplier {
 	private int currentChar = 0;
 	private int length = 0;
 	
-	public FileCodeSupplier(FileTextSupplier fileText) throws Exception {
+	public FileCodeSupplier(FileTextSupplier fileText, MacroMap macros) throws Exception {
 		this.fileText = fileText;
-		chars = new char[DEFAULT_CHAR_BUFFER_LENGTH]; //Start of with 20
-		lineNums = new int[DEFAULT_CHAR_BUFFER_LENGTH];
-		colNums = new int[DEFAULT_CHAR_BUFFER_LENGTH];
-		
+		this.macros = macros;
+		this.chars = new char[DEFAULT_CHAR_BUFFER_LENGTH];
+		this.lineNums = new int[DEFAULT_CHAR_BUFFER_LENGTH];
+		this.colNums = new int[DEFAULT_CHAR_BUFFER_LENGTH];
+	
 		if(!fileText.hasChar())
 			if(!fileText.advance())
-				log(FATAL_ERROR, "The %s given to a FileCodeSupplier refused to give a char or advance!", fileText.toString());
-		if(!fileText.hasChar())
-			log(FATAL_ERROR, "%s refused to give a char a second time!", fileText.toString());
+				log(FATAL_ERROR, "FileCodeSupplier was given a completly empty: %s", fileText.toString());
 		
-		chars   [length] = fileText.getCurrentChar();
-		lineNums[length] = fileText.getCurrentLine();
-		colNums [length] = fileText.getCurrentCol ();
+		if(!fileText.hasChar()) //Now it definitely *should* have one
+			log(FATAL_ERROR, "FileCodeSupplier was given broken %s", fileText.toString());
+		if(!appendChar(fileText.getCurrentChar(), fileText.getCurrentLine(), fileText.getCurrentCol()))
+			log(FATAL_ERROR, "FileCodeSupplier was given a FileTextSupplier that refued to give it's promised char away!");
+		if(!fileText.advance())
+			log(FATAL_ERROR, "FileCodeSupplier was given a one char long unadvanceable %s", fileText.toString());
+	}
+
+	private boolean appendChar(char c, int line, int col) {
+		if(length >= chars.length) {
+			if(currentBase > 0) {
+				deleteUnusedBuffer();
+			} else {
+				makeBufferBigger();
+			}
+		}
+		if(length >= chars.length) //Our size making measures failed
+			return false;
+		chars   [length] = c;
+		lineNums[length] = line;
+		colNums [length] = col;
 		length++;
+		return true;
+	}
+	
+	private void deleteUnusedBuffer() {
+		int newLength = length - currentBase;
+		currentChar -= currentBase;
+		log(SUPER_DEBUG, "Moving chars in FileCodeSupplier back to make more space. OldLength: %d, NewLength: %d", length, newLength);
+		length = newLength;
+		
+		System.arraycopy(chars,    currentBase, chars,    0, length);
+		System.arraycopy(lineNums, currentBase, lineNums, 0, length);
+		System.arraycopy(colNums,  currentBase, colNums,  0, length);
+		
+		currentBase = 0;
+	}
+	
+	private void makeBufferBigger() {
+		int newSize = (int)(chars.length*ARRAY_EXPAND_FACTOR);
+		
+		char[] oldChars = chars;
+		int[] oldLines = lineNums;
+		int[] oldCols = colNums;
+		chars = new char[newSize];
+		lineNums = new int[newSize];
+		oldCols = new int[newSize];
+		System.arraycopy(oldChars, 0, chars, 0, length);
+		System.arraycopy(oldLines, 0, lineNums, 0, length);
+		System.arraycopy(oldCols , 0, colNums, 0, length);
 	}
 	
 	@Override
@@ -43,89 +89,32 @@ public class FileCodeSupplier implements Supplier {
 
 	@Override
 	public boolean hasChar() {
-		return currentChar<length;
+		return currentChar < length;
 	}
 
 	@Override
 	public char getCurrentChar() {
-		if(currentChar<length)
-			return chars[currentChar];
-		log(ERROR, "FileCodeSupplier asked for chars when hasChar retruned false");
-		return '\n';
+		if(currentChar >= length)
+			log(ASSERTION_FAILED, "Asked for current char even though there is no current char!");
+		return chars[currentChar];
 	}
 
 	@Override
 	public int getCurrentLine() {
-		if(currentChar<length)
-			return lineNums[currentChar];
-		log(ERROR, "FileCodeSupplier asked for line number when hasChar retruned false");
-		return -1;
+		if(currentChar >= length)
+			log(ASSERTION_FAILED, "Asked for current char line even though there is no current char!");
+		return lineNums[currentChar];
 	}
-
+	
 	@Override
 	public int getCurrentCol() {
-		if(currentChar<length)
-			return colNums[currentChar];
-		log(ERROR, "FileCodeSupplier asked for line col when hasChar retruned false");
-		return -1;
+		if(currentChar >= length)
+			log(ASSERTION_FAILED, "Asked for current char col even though there is no current char!");
+		return colNums[currentChar];
 	}
 
 	@Override
 	public boolean advance() throws IOException {
-		if(currentChar<length-1) { //We just go on by one!
-			currentChar++; //current char may now be the last char
-			return true;
-		}
 		
-		//We need to go get some more chars!
-		
-		//As long as the char array isn't full, keep going
-		
-		if(length == chars.length) { //The array is full! Expand!
-			char[] oldChars    = chars;
-			int[]  oldLineNums = lineNums;
-			int[]  oldColNums  = colNums;
-			int newSize = (int)(oldChars.length*ARRAY_EXPAND_AMOUNT);
-			chars = new char[newSize];
-			lineNums = new int[newSize];
-			colNums = new int[newSize];
-			System.arraycopy(oldChars,    0, chars,    0, oldChars.length);
-			System.arraycopy(oldLineNums, 0, lineNums, 0, oldLineNums.length);
-			System.arraycopy(oldColNums,  0, colNums,  0, oldColNums.length);
-			log(DEBUG, "Had to expand char buffer in FileCodeSupplier from %d to %d", oldChars.length, chars.length);
-		}
-		
-		if(fileText.hasChar()) {
-			chars   [length] = fileText.getCurrentChar();
-			lineNums[length] = fileText.getCurrentLine();
-			colNums [length] = fileText.getCurrentCol ();
-			length++;
-			if(currentChar < length-1) //In the case we didn't have a char at all, we got one now
-				currentChar++;
-			if(chars[currentChar]==' ')
-				updateBaseToCurrent();
-			return fileText.advance(); //Out of text?
-		} else {
-			log(ERROR, "The FileTextSupplier is out of chars before advance() returned false");
-			return false;
-		}
 	}
-	
-	public void resetAtBase() {
-		currentChar = currentBase;
-	}
-
-	public void updateBaseToCurrent() {
-		
-		length = length - currentChar;
-		log(DEBUG, "Just update base of FileCodeSupplier! Length went from %d to %d!", length+currentChar, length);
-		
-		System.arraycopy(chars,    currentChar, chars,    0, length);
-		System.arraycopy(lineNums, currentChar, lineNums, 0, length);
-		System.arraycopy(colNums,  currentChar, colNums,  0, length);
-		
-		currentChar = 0;
-		currentBase = 0;
-	}
-	
 }
