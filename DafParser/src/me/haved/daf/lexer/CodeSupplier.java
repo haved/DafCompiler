7628a3c3879ba49945f44b3f0da71801df879d72
haved <krogstie.havard@gmail.com>
2016-04-10 22:51:32 +0200

    The world of suppliers is falling apart

--- DafParser/src/me/haved/daf/lexer/CodeSupplier.java
+++ DafParser/src/me/haved/daf/lexer/CodeSupplier.java
@@ -4,39 +4,33 @@ import me.haved.daf.RegisteredFile;
 
 import static me.haved.daf.LogHelper.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class CodeSupplier {
 	
 	private RegisteredFile file;
+	private BufferedReader fileReader;
 	
-	private FileChar[] bufferedFileChars;
+	private char[] bufferedChars;
+	private int[] bufferedLineNums;
+	private int[] bufferedColNums;
 	private int bufferIndex = 0;
-	
-	private FileChar currentFileChar = null;
 	
 	public CodeSupplier(RegisteredFile file) {
 		this.file = file;
+		
 	}
 	
 	public String getFileName() {
 		return file.fileName;
 	}
 	
-	public FileChar getCurrentChar() {
-		if(currentFileChar == null)
-			log(ASSERTION_FAILED, "Asked CodeSupplier for current char when there were none!");
-		return currentFileChar;
-	}
-	
-	/**
-	 * 
-	 */
-	public boolean advance() {
-		if(bufferIndex <= bufferedFileChars.length) {
-			currentFileChar = bufferedFileChars[bufferIndex];
-			bufferIndex++;
-			return true;
+	public void close() {
+		try {
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		return false;
 	}
 }
