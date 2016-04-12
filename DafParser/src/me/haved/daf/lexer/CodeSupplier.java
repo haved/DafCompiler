8522f5cfeec69aeb7d6626546ec6fa04e61b16f3
haved <krogstie.havard@gmail.com>
2016-04-12 22:13:09 +0200

    Add CodeSupplier code for design

--- DafParser/src/me/haved/daf/lexer/CodeSupplier.java
+++ DafParser/src/me/haved/daf/lexer/CodeSupplier.java
@@ -24,9 +24,37 @@ public class CodeSupplier {
 		
 		if(!fileText.hasChar())
 			log(ASSERTION_FAILED, "new FileTextSupplier was created, but has NO chars!");
-		current = fileText.getCurrentChar();
-		line = fileText.getCurrentLine();
-		col = fileText.getCurrentCol();
+		
+		if(!trySetCurrentChar(fileText.getCurrentChar(), fileText.getCurrentCol(), fileText.getCurrentLine())) //This should never be false because '\n'
+			log(ASSERTION_FAILED, "new FileTextSupplier gave a char that didn't fill the code supplier");
+	}
+	
+	private FileChar fc = new FileChar();
+	public boolean advance() {
+		while(true) {
+			if(!getNextChar(fc))
+				return false;
+			if(trySetCurrentChar(fc.c, fc.line, fc.col)) //If we didn't add a letter, try again!
+				return true;
+		}
+	}
+	
+	private boolean getNextChar(FileChar fc) {
+		fc.c++;
+		fc.col++;
+		return true;
+	}
+	
+	private boolean trySetCurrentChar(char c, int line, int col) {
+		
+		if(c=='/')
+			return false;
+		
+		current = c;
+		this.line = line;
+		this.col = col;
+		
+		return true;
 	}
 	
 	public String getFileName() {
@@ -47,5 +75,10 @@ public class CodeSupplier {
 	
 	public int getCurrentCol() {
 		return col;
+	}
+	
+	private static class FileChar {
+		public char c;
+		public int line, col;
 	}
 }
