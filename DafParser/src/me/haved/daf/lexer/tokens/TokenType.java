a75e1151c21aa941b6a3afe6f684be8e48e9d4e8
haved <krogstie.havard@gmail.com>
2016-05-09 23:12:12 +0200

    Add tiny amount of code to TokenType

--- DafParser/src/me/haved/daf/lexer/tokens/TokenType.java
+++ DafParser/src/me/haved/daf/lexer/tokens/TokenType.java
@@ -5,14 +5,22 @@ public enum TokenType {
 	NEW("new");
 	
 	private String text;
-	private boolean speacial;
+	private boolean special;
 	
 	TokenType(String text, boolean special) {
 		this.text = text;
-		this.speacial = special;
+		this.special = special;
 	}
 	
 	private TokenType(String text) {
 		this(text, false);
+	}
+	
+	public String getName() {
+		return text;
+	}
+	
+	public boolean isSpecial() {
+		return special;
 	}
 }
