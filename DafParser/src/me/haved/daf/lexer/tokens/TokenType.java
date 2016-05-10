13a517ab6be63ae701fb26c762338b6e06167fab
haved <krogstie.havard@gmail.com>
2016-05-10 23:19:29 +0200

    Add half of the tokens to TokenTypes

--- DafParser/src/me/haved/daf/lexer/tokens/TokenType.java
+++ DafParser/src/me/haved/daf/lexer/tokens/TokenType.java
@@ -2,7 +2,18 @@ package me.haved.daf.lexer.tokens;
 
 public enum TokenType {
 
-	NEW("new");
+	PUB, PROT, LET, MUT, DEF, UNCERTAIN, ASSIGN("="), COLON_ASSIGN(":="),
+	COLON(":"), SEMICOLON(";"), FUNC, PROC, INLINE, LEFT_PAREN("("),
+	COMMA(","), RIGHT_PAREN(")"), SCOPE_START("{"), SCOPE_END("}"),
+	
+	CLASS, ABSTRACT, EXTENDS, IMPLEMENTS, INTERFACE, METHOD, THIS,
+	CONST, VIRTUAL, OVERRIDE, DESTRUCTOR("~"),
+	
+	IF, ELSE, ELSELSE, FOR, WHILE, DO, CONTINUE, BREAK, RETRY, RETURN,
+	
+	CHAR, UBYTE, SHORT, USHORT, INT, UINT, LONG, ULONG,
+	INT8, UINT8, INT16, UINT16, INT32, UINT32, INT6, UINT64,
+	USIZE, BOOLEAN, FLOAT, DOUBLE;
 	
 	private String text;
 	private boolean special;
@@ -14,6 +25,11 @@ public enum TokenType {
 	
 	private TokenType(String text) {
 		this(text, false);
+	}
+	
+	private TokenType() {
+		this.text = name().toLowerCase();
+		this.special = false;
 	}
 	
 	public String getName() {
