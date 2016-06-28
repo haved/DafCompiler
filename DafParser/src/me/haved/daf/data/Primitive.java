c95a196b3c8ada9527a08f1f6791d457ed870cfb
haved <krogstie.havard@gmail.com>
2016-06-28 02:13:23 +0200

    Added a few primitives (gotta space 'em out)

--- DafParser/src/me/haved/daf/data/Primitive.java
+++ DafParser/src/me/haved/daf/data/Primitive.java
@@ -1,5 +1,17 @@
 package me.haved.daf.data;
 
-public class Primitive {
+import me.haved.daf.lexer.tokens.TokenType;
 
+public enum Primitive {
+	
+	UINT8(TokenType.UINT8, TokenType.UBYTE), 	INT8(TokenType.INT8, TokenType.CHAR),
+	UINT16(TokenType.UINT16, TokenType.USHORT),	INT16(TokenType.INT16, TokenType.SHORT);
+	
+	private String name;
+	private TokenType[] tokens;
+	
+	private Primitive(TokenType...tokens) {
+		this.name = tokens[0].getText();
+		this.tokens = tokens;
+	}
 }
