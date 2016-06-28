7879e3876a63d761135b1eacd3fe4980b2099ddc
haved <krogstie.havard@gmail.com>
2016-06-28 02:16:04 +0200

    Add getters to primitives

--- DafParser/src/me/haved/daf/data/Primitive.java
+++ DafParser/src/me/haved/daf/data/Primitive.java
@@ -5,7 +5,8 @@ import me.haved.daf.lexer.tokens.TokenType;
 public enum Primitive {
 	
 	UINT8(TokenType.UINT8, TokenType.UBYTE), 	INT8(TokenType.INT8, TokenType.CHAR),
-	UINT16(TokenType.UINT16, TokenType.USHORT),	INT16(TokenType.INT16, TokenType.SHORT);
+	UINT16(TokenType.UINT16, TokenType.USHORT),	INT16(TokenType.INT16, TokenType.SHORT),
+	UINT32(TokenType.UINT32, TokenType.UINT),	INT32(TokenType.INT32, TokenType.INT);
 	
 	private String name;
 	private TokenType[] tokens;
@@ -13,5 +14,16 @@ public enum Primitive {
 	private Primitive(TokenType...tokens) {
 		this.name = tokens[0].getText();
 		this.tokens = tokens;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean fitsTokenType(TokenType type) {
+		for(int i = 0; i < tokens.length; i++)
+			if(tokens[i]==type)
+				return true;
+		return false;
 	}
 }
