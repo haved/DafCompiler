bdaf0692ff07ab61cfb7224f6f49b413bca98cda
haved <krogstie.havard@gmail.com>
2016-07-09 13:37:47 +0300

    Filled shell of OperatorExpression

--- DafParser/src/me/haved/daf/data/expression/ExpressionInfixOperator.java
+++ DafParser/src/me/haved/daf/data/expression/ExpressionInfixOperator.java
@@ -3,9 +3,25 @@ package me.haved.daf.data.expression;
 import me.haved.daf.lexer.tokens.TokenType;
 
 public enum ExpressionInfixOperator {
-	PLUS(TokenType.PLUS), MINUS(TokenType.MINUS);
+	PLUS(TokenType.PLUS), MINUS(TokenType.MINUS), MULT(TokenType.MULT), DIVIDE(TokenType.DIVIDE), MODULO(TokenType.MODULO);
 	
-	ExpressionInfixOperator(TokenType token) {
-		
+	private TokenType token;
+	private boolean boolOperator;
+	
+	ExpressionInfixOperator(TokenType token, boolean boolOperator) {
+		this.token = token;
+		this.boolOperator = boolOperator;
+	}
+	
+	private ExpressionInfixOperator(TokenType token) {
+		this(token, false);
+	}
+	
+	public boolean matchesToken(TokenType type) {
+		return this.token == type;
+	}
+	
+	public boolean evaluatesToBoolean() {
+		return boolOperator;
 	}
 }
--- DafParser/src/me/haved/daf/data/expression/OperatorExpression.java
+++ DafParser/src/me/haved/daf/data/expression/OperatorExpression.java
@@ -1,22 +1,37 @@
 package me.haved.daf.data.expression;
 
 import me.haved.daf.data.NodeBase;
+import me.haved.daf.data.Primitive;
+import me.haved.daf.data.PrimitiveType;
 import me.haved.daf.data.Type;
 
 public class OperatorExpression extends NodeBase implements Expression {
-
+	private static final PrimitiveType IMMUTABLE_BOOLEAN = new PrimitiveType(Primitive.BOOLEAN, false);
+	
+	private Exception a, b;
+	private ExpressionInfixOperator operator;
+	
+	private boolean typeEvaluated;
+	private Type type;
+	
+	public OperatorExpression(Exception a, Exception b, ExpressionInfixOperator operator) {
+		this.a = a;
+		this.b = b;
+		this.operator = operator;
+		if(operator.evaluatesToBoolean()) {
+			this.typeEvaluated = true;
+			this.type = IMMUTABLE_BOOLEAN;
+		}
+	}
 	
 	
 	@Override
 	public boolean isTypeSet() {
-		// TODO Auto-generated method stub
-		return false;
+		return typeEvaluated;
 	}
 
 	@Override
 	public Type getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return type;
 	}
-
 }
