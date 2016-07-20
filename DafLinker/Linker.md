650b4d3b8aac674208eb58408536507be0445811
haved <krogstie.havard@gmail.com>
2016-07-20 23:59:10 +0200

    Progress on linker

--- DafLinker/Linker.md
+++ DafLinker/Linker.md
@@ -4,30 +4,32 @@ A Linkfile contains parameters written by the daf compiler while compiling each 
 Daf headers for libraries can contain the keyword `linkfile "-SDL2";` or `linkfile "daf/memory.o"` to append the object file or library to the Linkfile.
 The executable for the daf linker is called *dafln*, and has standard search directories defined in `/usr/share/daf/linker_search.txt` and `$HOME/.daf/linker_search.txt`.
   
-NOTE: The linker_search.txt files are parsed like Linkfiles, so `-I` and `-L` is required before the directory path
+NOTE: The linker_search.txt files are parsed like Linkfiles, so `-I` or `-L` is required before the directory path
 
 ##### Format and usage:
 ```
 Usage: dafln <OPTION LIST>
 
 List entries:
-    -F <file>:   Load a linkfile
-    <A file>:    Added as an object file from a search directory.
-    -l<library>: Adds a library from a search directory.
-    -I <dir>:    Add a search directory for object files
-    -L <dir>:    Add a search directory for linker libraries
-    -o <file>:   Set output file name
-    -X <linker>: Set the linker program used
-    -x <arg>:    Pass a single arg to the linker
-    -h --help    Print this help message
+    -F <file>:      Load a linkfile
+    <A file>:       Added as an object file from a search directory.
+    -l<library>:    Adds a library from a library search directory.
+    -I <dir>:       Add a search directory for object files
+    -L <dir>:       Add a search directory for linker libraries
+    -o <file>:      Set output file name
+    -X <linker>:    Set the linker program used (default: ld)
+    -x <arg>:       Pass a single arg to the linker
+    -rpath <dir>:   Set runtime shared library search directory
+    -h --help       Print this help message
 ```
 
-The current working directory is also a search directory when looking for object files and libraries
+The current working directory is also a search directory when looking for object files and libraries.
 The linkfile is just the same option list.
 When loading a linkfile, the relative search directory is temporarily set there, but the output will allways be relative to the working directory.
 Everything behind `#` on a line is a comment.
 
 Default behaviour:
+ - All library search locations 
  - If no object files or libraries are supplied, dafln will automaticly look for and load *./Linkfile*
  - If no linker is supplied, *ld* will be used
  - If no output file is supplied, *daf.out* is used
--- DafLinker/dafln.py
+++ DafLinker/dafln.py
@@ -5,7 +5,7 @@ linker_search_files = ["/usr/share/daf/linker_search.txt", expanduser("~/.daf/li
 
 from sys import argv
 from subprocess import call
-from os.path import dirname, realpath
+from os.path import dirname, realpath, split
 from os import chdir, getcwd
 
 object_files = []
@@ -21,29 +21,49 @@ def printHelpMessage():
  """Usage: dafln <OPTION LIST>
 
 List entries:
-    -F <file>:   Load a linkfile
-    <A file>:    Added as an object file from a search directory.
-    -l<library>: Adds a library from a search directory.
-    -I <dir>:    Add a search directory for object files
-    -L <dir>:    Add a search directory for linker libraries
-    -o <file>:   Set output file name
-    -X <linker>: Set the linker program used
-    -x <arg>:    Pass a single arg to the linker
-    -h --help    Print this help message
+    -F <file>:      Load a linkfile
+    <A file>:       Added as an object file from a search directory.
+    -l<library>:    Adds a library from a library search directory.
+    -I <dir>:       Add a search directory for object files
+    -L <dir>:       Add a search directory for linker libraries
+    -o <file>:      Set output file name
+    -X <linker>:    Set the linker program used (default: ld)
+    -x <arg>:       Pass a single arg to the linker
+    -rpath <dir>:   Set runtime shared library search directory
+    -h --help       Print this help message
     """)
+    exit();
+
+def getArg(args, index):
+    if index >= len(args):
+        print(args[index-1][1]+":", args[index-1][2]+": error: expected something after", args[index-1][0])
+        return None
+    return args[index]
+
+def addSearchDir(dir): #Only used for object file serch dirs
+    o_search_dirs.append(dir[0])
+
+def addLibrarySearchDir(dir):
+    lib_search_dirs.append(dir[0])
+
+def addObjectFile(arg):
+    pass
 
 def handleFile(filePath):
     try:
         file = open(filePath);
+        fileName = split(filePath)[1]
         args = []
+        lineNum = 0
         while True:
+            lineNum+=1
             line = file.readline()
             if len(line) == 0:
                 break;
 
-            for arg in line.split('\n')[0].split(' '):
+            for arg in line.split('#')[0].split('\n')[0].split(' '):
                 if len(arg) > 0:
-                    args.append(arg)
+                    args.append((arg, fileName, lineNum))
             
         chdir(dirname(filePath))
         handleParameters(args)
@@ -54,12 +74,21 @@ def handleFile(filePath):
 def handleParameters(args):
     i = 0
     while i < len(args):
-        arg = args[i]
+        arg = args[i][0]
         if arg == "--help":
             printHelpMessage()
+        elif arg == "-I":
+            i+=1
+            addSearchDir(getArg(args, i))
+        elif arg == "-L":
+            i+=1
+            addLibrarySearchDir(getArg(args, i))
+        else:
+            addObjectFile(args[i])
+        i+=1
 
 def main() :
-    global output, linker, linker_args, object_files, libraries
+    global object_files, libraries, lib_search_dirs, output, linker, linker_args
 
     main_wd = getcwd();
     for linker_search in linker_search_files:
@@ -72,10 +101,13 @@ def main() :
         linker = "ld"
 
     args = [linker]+linker_args
-    for dir in lib_dirs:
+    for dir in lib_search_dirs:
         args+=["-L", dir]
     args += object_files+libraries+["-o", output]
-    print(args);
+    
+    for arg in args:
+        print(arg, end=" ")
+    print("")
 
     retcode = call(args)
 
