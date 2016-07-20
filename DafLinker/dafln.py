8736d3d002308401aeede176e24f93c74c53fd37
haved <krogstie.havard@gmail.com>
2016-07-21 00:36:11 +0200

    Get Linkfile recursion to work

--- DafLinker/dafln.py
+++ DafLinker/dafln.py
@@ -2,10 +2,11 @@
 from os.path import expanduser
 
 linker_search_files = ["/usr/share/daf/linker_search.txt", expanduser("~/.daf/linker_search.txt")]
+maxLevel = 10
 
 from sys import argv
 from subprocess import call
-from os.path import dirname, realpath, split
+from os.path import dirname, realpath, split, join
 from os import chdir, getcwd
 
 object_files = []
@@ -36,7 +37,7 @@ List entries:
 
 def getArg(args, index):
     if index >= len(args):
-        print(args[index-1][1]+":", args[index-1][2]+": error: expected something after", args[index-1][0])
+        print(args[index-1][1]+":", str(args[index-1][2])+": error: expected something after", args[index-1][0])
         return None
     return args[index]
 
@@ -49,11 +50,20 @@ def addLibrarySearchDir(dir):
 def addObjectFile(arg):
     pass
 
-def handleFile(filePath):
+def findAndHandleFile(arg, level):
+    if handleFile(arg[0], level) == False:
+        print(arg[1]+":", str(arg[2])+": error: File not found:", arg[0])
+
+def handleFile(filePath, level):
+    if level > maxLevel:
+        print("Aborting: Already", level, "levels in. Recursion?")
+        exit()
+    wd = getcwd()
+    filePath = join(wd, filePath)
+    args = []
     try:
         file = open(filePath);
         fileName = split(filePath)[1]
-        args = []
         lineNum = 0
         while True:
             lineNum+=1
@@ -64,25 +74,37 @@ def handleFile(filePath):
             for arg in line.split('#')[0].split('\n')[0].split(' '):
                 if len(arg) > 0:
                     args.append((arg, fileName, lineNum))
-            
-        chdir(dirname(filePath))
-        handleParameters(args)
+        file.close()
     except FileNotFoundError as e:
         return False;
-    return True;
+    chdir(dirname(filePath))
+    handleParameters(args, level)
+    chdir(wd)
+    return True
 
-def handleParameters(args):
+def handleParameters(args, level):
     i = 0
     while i < len(args):
         arg = args[i][0]
         if arg == "--help":
             printHelpMessage()
+        elif arg == "-F":
+            i+=1
+            findAndHandleFile(getArg(args, i), level+1)
         elif arg == "-I":
             i+=1
             addSearchDir(getArg(args, i))
         elif arg == "-L":
             i+=1
             addLibrarySearchDir(getArg(args, i))
+        elif arg[:2] == "-l":
+            libraries.append(arg)
+        elif arg == "-o":
+            i+=1
+            newOut = getArg(args, i)
+            if output != None:
+                print(newOut[1]+":", str(newOut[2])+": warning: Setting output name again! (from '", output, "' to '", newOut[0], "')")
+            output = newOut[0]
         else:
             addObjectFile(args[i])
         i+=1
@@ -90,10 +112,8 @@ def handleParameters(args):
 def main() :
     global object_files, libraries, lib_search_dirs, output, linker, linker_args
 
-    main_wd = getcwd();
     for linker_search in linker_search_files:
-        handleFile(linker_search)
-    chdir(main_wd) #To make the output happen at cwd
+        handleFile(linker_search, 0)
 
     if output == None:
         output = "daf.out"
@@ -110,6 +130,7 @@ def main() :
     print("")
 
     retcode = call(args)
+    exit(retcode)
 
 if __name__ == "__main__":
     main()
