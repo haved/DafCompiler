629cff0e0c5902d0ac87563162e81c4550ce4c51
haved <krogstie.havard@gmail.com>
2016-07-23 11:14:03 +0200

    Add many arguments to dafln.py

--- DafLinker/dafln.py
+++ DafLinker/dafln.py
@@ -2,6 +2,8 @@
 
 from os.path import expandvars
 from sys import argv
+from os import getcwd, chdir
+from os.path import join, split, isfile, isdir
 
 defaultLinkfilePaths = ["/usr/share/daf/linker_search.txt", expandvars("$HOME/.daf/linker_search.txt")]
 defaultLinkerType = "gnu_link_linux"
@@ -9,20 +11,7 @@ defaultLinkfile = "Linkfile"
 
 exec_name = argv[0]
 
-class Input:
-    def __init__(self):
-        self.object_files = []
-        self.libraries = []
-        self.object_file_search = []
-        self.library_search = []
-        self.outputFile = None
-        self.type = None
-        self.executable = None
-    
-    def makeArgumentList(self):
-        return self.type.makeArgumentList(self)
-
-class Parameter:
+class Argument:
     def __init__(self, text, file, line):
         self.text = text
         self.file = file
@@ -40,29 +29,128 @@ def getLinkerType(text):
     if text == "gnu_link_linux":
         return GnuLinkerLinux()
     return None
+#
+def log(file, line, level, text):
+    if line > 0:
+        print(file,":",line,": ",level,": ",text,sep='')
+    else:
+        print(file,": ",level,": ",text,sep='')
+def logArg(arg, level, text):
+    log(arg.file, arg.line, level, text)
+def logError(arg, text):
+    logArg(arg, "error", text)
+def logWarning(arg, text):
+    logArg(arg, "warning", text)
+#
 
-from os import getcwd, chdir
-from os.path import join, split
+def getArg(args, index):
+    if index >= len(args):
+        logError(args[index-1], "Expected something after " + args[index-1].text)
+        exit(1);
+    return args[index]
+def getFullPath(arg):
+    if isdir(arg.text):
+        return join(getcwd(), arg.text)
+    else:
+        logWarning(arg, "Directory '"+join(getcwd(), arg.text)+"' not found'")
+    return None
+def appendIfReal(list, elm):
+    if elm != None:
+        list.append(elm)
+
+class Input:
+    def __init__(self):
+        self.object_files = []
+        self.libraries = []
+        self.object_file_search = []
+        self.library_search = []
+        self.objWhitelist = []
+        self.outputFile = None
+        self.type = None
+        self.executable = None
+
+    def makeArgumentList(self):
+        return self.type.makeArgumentList(self)
+
+    def addObjectFile(self, arg):
+        for dir in self.object_file_search:
+            path = join(dir, arg.text)
+            if isfile(path):
+                self.object_files.append(path)
+                return True
+        if isfile(arg.text):
+            self.object_files.append(join(getcwd(), arg.text))
+            return True
+        logError(arg, "Object file "+arg.text+" wasn't found in any search directory.")
+
+    def addLibrary(self, arg):
+        self.libraries.append(arg.text)
+
+    def addObjectFileSearchDir(self, arg):
+        appendIfReal(self.object_file_search, getFullPath(arg))
+
+    def addLibrarySearchDir(self, arg):
+        appendIfReal(self.library_search, getFullPath(arg))
+
+    def addDirToObjectWhitelist(self, arg):
+        appendIfReal(self.objWhitelist, getFullPath(arg))
+
+    def handleArguments(self, args):
+        index = 0
+        while index < len(args):
+            arg = args[index]
+            argText = arg.text
+            if argText == "-F":
+                index+=1
+                handleFile(getArg(args, index).text, self)
+            elif argText[:2] == "-l":
+                self.addLibrary(arg)
+            elif argText == "-I":
+                index += 1
+                self.addObjectFileSearchDir(getArg(args, index))
+            elif argText == "-L":
+                index += 1
+                self.addLibrarySearchDir(getArg(args, index))
+            elif argText == "-A":
+                index += 1
+                self.addDirToObjectWhitelist(getArg(args, index))
+            elif argText == "-o":
+                index += 1
+                self.outputFile = getArg(args, index)
+            elif arg
+            else:
+                newIndex = self.type.parseArgument(args, index)
+                if newIndex != None and newIndex >= 0:
+                    index = newIndex
+                else:
+                    self.addObjectFile(arg)
+            index += 1
 
 def handleFile(filePath, input):
-    params = []
+    args = []
+    fileName = split(filePath)[1]
+
     try:
         file = open(filePath)
+        lineC = 0
         while True:
+            lineC += 1
             line = file.readline()
             if len(line)==0:
                 break
-            params += line.split('#')[0].split('\n')[0].split(" ")
+            for arg in line.split('#')[0].split('\n')[0].split(" "):
+                if len(arg)>0:
+                    args.append(Argument(arg, exec_name, lineC))
     except FileNotFoundError as e:
         return False
+    if len(args) == 0:
+        return True
     wd = getcwd()
-    chdir(split(join(wd,filePath))[0])
-    handleParameters(params, input)
+    chdir(split(filePath)[0])
+    input.handleArguments(args)
     chdir(wd)
     return True
 
-def handleParameters(parameters, input):
-    pass
 
 
 from subprocess import call
@@ -72,11 +160,11 @@ def main():
     input.type = getLinkerType(defaultLinkerType)
     input.executable = input.type.getDefaultExecutable()
     assert(input.type != None)
-       
+
     for linkerfile in defaultLinkfilePaths:
         handleFile(linkerfile, input)
 
-    handleParameters([Parameter(arg, exec_name, -1) for arg in argv[:-1]], input)
+    input.handleArguments([Argument(arg, exec_name, -1) for arg in argv[1:]])
 
     args = input.makeArgumentList()
 
