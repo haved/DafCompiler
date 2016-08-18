f359257c8324561480b323cad42064a381d7f851
haved <krogstie.havard@gmail.com>
2016-08-18 12:15:14 +0200

    Add import definition signature printing

--- DafParser/src/me/haved/daf/data/definition/ImportDefinition.java
+++ DafParser/src/me/haved/daf/data/definition/ImportDefinition.java
@@ -6,14 +6,32 @@ import me.haved.daf.data.NodeBase;
 
 public class ImportDefinition extends NodeBase implements Definition {
 	private String[] parts;
+	private String cppLibName;
+	private boolean cppLib;
 	
 	public ImportDefinition(String[] parts) {
 		this.parts = parts;
+		this.cppLib = false;
+	}
+	
+	public ImportDefinition(String name) {
+		this.cppLibName = name;
+		this.cppLib = true;
 	}
 	
 	@Override
 	public String getSignature() {
-		return null;
+		if(cppLib)
+			return String.format("import \"%s\";", cppLibName);
+		StringBuilder out = new StringBuilder();
+		out.append("import ");
+		for(int i = 0; i < parts.length; i++) {
+			if(i!=0)
+				out.append(".");
+			out.append(parts[i]);
+		}
+		out.append(";");
+		return out.toString();
 	}
 
 	@Override
