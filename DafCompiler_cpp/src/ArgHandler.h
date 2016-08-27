3a78493d97f8ecb091132151e2f9be433b9d91ca
haved <krogstie.havard@gmail.com>
2016-08-27 11:15:37 +0200

    Rafactor command argument parsing to another file

---
+++ DafCompiler_cpp/src/ArgHandler.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
+using std::vector;
+
+struct FileForParsing {
+    fs::path inputFile;
+    fs::path outputFile;
+    bool outputFileSet;
+    bool recursive;
+    bool fullParse; //If there is to be an output
+    unsigned int ID;
+    FileForParsing(const fs::path& inputFile, const fs::path& outputFile, bool outputFileSet, bool recursive, bool fullParse);
+};
+
+vector<FileForParsing> parseParameters(int argc, const char** argv);
