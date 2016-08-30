32ad70efc5921c7eebc68b07902383bcda9cef1d
haved <krogstie.havard@gmail.com>
2016-08-30 20:28:22 +0200

    Add scanning of file

--- DafCompiler_cpp/src/parsing/Lexer.cpp
+++ DafCompiler_cpp/src/parsing/Lexer.cpp
@@ -14,15 +14,37 @@ Lexer::Lexer(const fs::path& file) : currentToken(token1), lookaheadToken(token2
     advance(); //To make current an actual token
 }
 
+bool isWhitespace(char c) {
+    return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+}
+
+inline bool isEOF(char c) {
+    return c == EOF;
+}
+
 bool Lexer::advance() {
     std::swap(currentToken, lookaheadToken);
     //Now set the look-ahead token
     while(true) {
-
+        if(isWhitespace(currentChar))
+            advanceChar();
+        else if(isEOF(currentChar)) {
+            lookaheadToken.type = END;
+            return false;
+        }
+        else {
+            lookaheadToken.type = PROT;
+            advanceChar();
+            return true;
+        }
     }
 }
 
 void Lexer::advanceChar() {
     std::swap(currentChar, lookaheadChar);
+    if(infile.eof())
+        lookaheadChar = EOF;
+    else
+        infile.get(lookaheadChar);
 }
 
--- DafCompiler_cpp/src/parsing/Parser.cpp
+++ DafCompiler_cpp/src/parsing/Parser.cpp
@@ -1,8 +1,17 @@
 #include "parsing/Parser.h"
+#include "parsing/Lexer.h"
+#include <iostream>
 
  std::unique_ptr<ParsedFile> parseFileSyntax(const fs::path& destFile, bool fullParse) {
     auto file = std::make_unique<ParsedFile>();
     file->fullyParsed = fullParse;
+    Lexer lexer(destFile);
+    do {
+        std::cout << lexer.getCurrentToken().type << std::endl;
+        if(lexer.getCurrentToken().type == END)
+            break;
+        lexer.advance();
+    } while(true);
     //Make lexer for file
     //Go through file and parse syntax
     //If !fullParse:
