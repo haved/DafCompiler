0da2f18f09d0f846cdee9a1e9063565d37810ba8
haved <krogstie.havard@gmail.com>
2016-09-14 15:25:56 +0200

    Add TextRange class

--- DafCompiler_cpp/src/parsing/DefinitionParser.cpp
+++ DafCompiler_cpp/src/parsing/DefinitionParser.cpp
@@ -1,2 +1,16 @@
 #include "parsing/DefinitionParer.h"
 
+TextRange::TextRange(int lineStart, int colStart, int lineEnd, int colEnd) {
+    this->lineStart = lineStart;
+    this->colStart = colStart;
+    this->lineEnd = lineEnd;
+    this->colEnd = colEnd;
+}
+
+TextRange::TextRange(TextRange& start, TextRange& rangeEnd) {
+    this->lineStart = start.lineStart;
+    this->colStart = start.colStart;
+    this->lineEnd = start.lineEnd;
+    this->colEnd = start.colEnd;
+}
+
--- DafCompiler_cpp/src/parsing/DefinitionParser.h
+++ DafCompiler_cpp/src/parsing/DefinitionParser.h
@@ -1,6 +1,19 @@
 #pragma once
 #include <memory>
 
+class TextRange {
+    private:
+        int lineStart;
+        int colStart;
+        int lineEnd;
+        int colEnd;
+    public:
+        TextRange(int lineStart, int colStart, int lineEnd, int colEnd);
+        TextRange(TextRange& start, TextRange& rangeEnd);
+        TextRange();
+};
+
 class Definition {
+    TextRange range;
 
 };
