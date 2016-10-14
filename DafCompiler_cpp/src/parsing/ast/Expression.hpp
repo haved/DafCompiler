3ffdbbe781435707da9581ebeda8c6283d933f11
haved <krogstie.havard@gmail.com>
2016-10-14 17:28:40 +0200

    Add Function type

--- DafCompiler_cpp/src/parsing/ast/Expression.hpp
+++ DafCompiler_cpp/src/parsing/ast/Expression.hpp
@@ -14,7 +14,7 @@ class Expression {
   virtual ~Expression();
   virtual bool isStatement();
   virtual const Type& getType();
-  inline bool isTypeKnown() {
+  virtual bool isTypeKnown() {
     return (bool)m_type;
   }
   virtual bool findType()=0;
@@ -70,52 +70,14 @@ private:
   std::string m_text;
 };
 
-//TODO: Move to Function Type
-enum FunctionParameterReferenceType {
-  FUNC_PARAM_BY_VALUE,
-  FUNC_PARAM_BY_REF,
-  FUNC_PARAM_BY_MUT_REF,
-  FUNC_PARAM_BY_MOVE,
-};
-
-class FunctionParameter {
-private:
-  FunctionParameterReferenceType m_ref_type;
-  std::string m_name;
-  shared_ptr<Type> m_type;
-};
-
-class CompileTimeParameter {
-public:
-  virtual ~CompileTimeParameter();
-};
-
-class TypeCompileTimeParameter : public CompileTimeParameter {
-  
-};
-
-class DefCompileTimeParameter : public CompileTimeParameter {
-  
-};
-
-//TODO: Give to FunctionType instead
-enum FunctionInlineType {
-  NORMAL_FUNCTION, INLINE_FUNCTION, TRUE_INLINE_FUNCTION
-};
-
-//TODO: Replace with def definitions own enum
-enum FunctionReturnType {
-  NORMAL_RETURN, LET_RETURN, MUT_RETURN
-};
-
-//TODO: Merge somewhat with the type definition for functions
 class FunctionExpression : public Expression {
 private:
-  std::vector<CompileTimeParameter> m_cmp_parameters;
-  std::vector<FunctionParameter> m_parameters;
-  FunctionInlineType m_inlineType;
-  FunctionReturnType m_returnType;
-  //TODO: Insert function body
+  FunctionType m_function;
+  Expression m_body;
 public:
   FunctionExpression();
+  bool findType() {return false;}
+  bool isTypeKnown() {return true;}
+  Type* getType() {return &m_function;}
+  void printSignature();
 };
--- DafCompiler_cpp/src/parsing/ast/Type.hpp
+++ DafCompiler_cpp/src/parsing/ast/Type.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <memory>
+#include <vector>
 
 class Type {
 public:
@@ -8,6 +10,7 @@ public:
   virtual void printSignature()=0;
 };
 
+//A type being defined by a typedef
 class TypedefType : public Type {
 private:
   std::string m_name;
@@ -16,3 +19,56 @@ public:
   ~TypedefType();
   void printSignature();
 };
+
+enum FunctionParameterReferenceType {
+  FUNC_PARAM_BY_VALUE,
+  FUNC_PARAM_BY_REF,
+  FUNC_PARAM_BY_MUT_REF,
+  FUNC_PARAM_BY_MOVE
+};
+
+class FunctionParameter {
+private:
+  FunctionParameterReferenceType m_ref_type;
+  std::string m_name;
+  std::shared_ptr<Type> m_type;
+};
+
+enum CompileTimeParameterType {
+  COMP_FUNC_PARAM_TYPE,
+  COMP_FUNC_PARAM_DEF,
+  COMP_FUNC_PARAM_LET,
+  COMP_FUNC_PARAM_MUT
+};
+
+class CompileTimeFunctionParameter {
+private:
+  CompileTimeParameterType m_compParamType;
+  std::string m_name;
+  std::shared_ptr<Type> m_type;
+};
+
+enum FunctionInlineType {
+  NORMAL_FUNCTION, INLINE_FUNCTION, TRUE_INLINE_FUNCTION
+};
+
+enum FunctionReturnType {
+  NORMAL_RETURN, LET_RETURN, MUT_RETURN
+};
+
+class FunctionType : public Type {
+private:
+  std::vector<CompileTimeFunctionParameter> m_compileParameters;
+  std::vector<FunctionParameter> m_parameters;
+  FunctionInlineType m_inlineType;
+  std::shared_ptr<Type> m_returnType;
+  FunctionReturnType m_returnTypeType;
+public:
+  FunctionType(std::vector<CompileTimeFunctionParameter> cpmParams,
+               std::vector<FunctionParameter> params,
+               FunctionInlineType inlineType,
+               std::shared_ptr<Type> returnType,
+               FunctionReturnType returnTypeType);
+};
+
+
