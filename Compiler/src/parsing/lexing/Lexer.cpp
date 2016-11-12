cf06cefbe8fdf79c027ec5d403a2286cb53c08c3
haved <krogstie.havard@gmail.com>
2016-11-12 02:33:07 +0100

    Add lots of checks for correct number literal parsing. Should probably have anti-tests

--- Compiler/src/parsing/lexing/Lexer.cpp
+++ Compiler/src/parsing/lexing/Lexer.cpp
@@ -106,18 +106,44 @@ bool Lexer::parseNumberLiteral(Token& token) {
     }
   }
 
-  if(realNumber) {
-    if((base == 10 && currentChar == 'e') || (base == 16 && currentChar == 'p')) {
-      do { //Ey, using do while :)
-        text.push_back(currentChar);
-        advanceChar();
-      } while(isDigit(currentChar) || (base==16&&isHexaDigit(currentChar)));
-    }
+  bool p;
+  while((p = (currentChar == 'p'))) { //We go until we don't have a 'p'
+    if(!realNumber)
+      logDaf(getFile(), line, col, ERROR) << "'p' indicating power of two after an integer" << std::endl;
+    if(base == 10)
+      logDaf(getFile(), line, col, ERROR) << "'p' indicating power of two after base 10 real" << std::endl;
+    else
+      break; //...or we find out we are allowed to have a 'p'
+    advanceChar();
+  }
+
+  bool eB_N10 = currentChar == 'e' && base != 10; //If we have an 'e' but are not allowed
+  while(eB_N10) {
+    logDaf(getFile(), line, col, ERROR) << "'e' following non base 10 " << (realNumber?"real":"integer") << std::endl;
+    advanceChar();
+    eB_N10 = currentChar == 'e'; //We already know the base is not 10
+  }
+
+  if(currentChar == 'e' || p) { //We are allowed to push them if they are current
+    do { //Ey, using do while :)
+      text.push_back(currentChar);
+      advanceChar(); //Eat [ep0-9]
+    } while(isDigit(currentChar));
   }
 
   char type='\0';
   int size = 32;
-  if(realNumber ? currentChar == 'f' : currentChar == 'i' || currentChar == 'u') {
+  if(realNumber ? currentChar == 'i' || currentChar == 'u' : currentChar == 'f') {
+    logDaf(getFile(), startLine, startCol, ERROR) << "Mismatch between " << (realNumber ? "real number" : "integer") << " and type " << currentChar << std::endl;
+    advanceChar(); //Eat botched type on the end
+    if(currentChar == '1' || currentChar == '3' || currentChar == '6') {
+      advanceChar(); //Eat [136]
+      advanceChar(); //Eat [624], or anything else, really
+    }
+    if(currentChar == '8')
+      advanceChar(); //Eat '8'
+  }
+  else if(currentChar == 'f' || currentChar == 'i' || currentChar == 'u') {
     type = currentChar;
     advanceChar(); //Eat 'type'
 
@@ -142,32 +168,49 @@ bool Lexer::parseNumberLiteral(Token& token) {
       next = '?';
       break;
     }
-    advanceChar();
+    advanceChar(); //Eat first part of type size
     if(next=='?') {
       auto &out = logDaf(getFile(), line, col, ERROR) << "When specifying " << (type=='f'?"real":type=='i'?"signed integer":"unsigned integer") << " literal type, use ";
       if (type != 'f')
         out << type << "8, " << type << "16, ";
       out << type << "32, or " << type << "64" << std::endl;
       type = '\0';
-      //Size is 32 and type is back to '\0'
+      //Size is default and type is back to '\0'
     }
-    else if(next != '\0');
+    else if(next == '\0') {}
     else if(currentChar != next) {
-      logDaf(getFile(), line, col, ERROR) << "Expected '" << next << "' before '" << currentChar << "' when parsing number literal type \"" << type << size << std::endl;
-    } else {
-      advanceChar(); //Eat second digit in type size;
+      logDaf(getFile(), line, col, ERROR) << "Expected '" << next << "' before '" << currentChar << "' when parsing number literal type '" << type << size << "'" << std::endl;
+      if(isDigit(currentChar))
+        advanceChar();
+    }
+    else
+      advanceChar(); //Eat second part of type size
+
+    { //Eat any digits or  left
+      bool printed = false;
+      std::ostream* out;
+      while(isHexaDigit(currentChar) || currentChar == 'u' || currentChar == 'i' || currentChar == 'p') {
+        if(!printed) {
+          out = &(logDaf(getFile(), line, col, ERROR) << "Too many symbols following number literal: ");
+          printed = true;
+        }
+        *out << currentChar;
+        advanceChar(); //Eat the rest of digits in type size;
+      }
+      if(printed)
+         *out << std::endl;
     }
   }
 
-  if(realNumber && type == '\0')
+  if(realNumber && type == '\0') //Give floats without type the float type
     type = 'f';
-  if(realNumber && type != 'f') {
+  if(realNumber && type != 'f') { //If a real number has something else, it's bad
     logDaf(getFile(), line, col, ERROR) << "A floating point number can't be marked with '" << type << size << '\'' << std::endl;
     type = 'f';
     if(size != 32 && size != 64)
       size = 32;
   }
-  else if(type == 'f' && size != 32 && size != 64) {
+  else if(type == 'f' && size != 32 && size != 64) { //A float thats something else than 32 or 64 bits is bad
     logDaf(getFile(), line, col, ERROR) << "A floating point number can only be of sizes f32 and f64" << std::endl;
     size = 32;
   }
