24568cdb67c6d1f1c3956aacb3c939360f98d91d
haved <krogstie.havard@gmail.com>
2017-01-31 19:48:36 +0100

    Retab Expression.hpp og Scope.cpp

--- Compiler/src/parsing/ast/Expression.hpp
+++ Compiler/src/parsing/ast/Expression.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "parsing/ast/TextRange.hpp"
 #include "parsing/ast/Type.hpp"
 #include "info/PrimitiveSizes.hpp"
@@ -13,119 +14,119 @@ using std::unique_ptr;
 class Lexer;
 
 class Expression {
- public:
-  Expression(const TextRange& range);
-  virtual ~Expression();
-  virtual bool isStatement();
-  virtual bool needsSemicolonAfterStatement();
-  virtual const Type& getType();
-  virtual bool isTypeKnown();
-  //returns true if it has a type after the call
-  virtual bool findType() = 0;
-  virtual void printSignature() = 0;
-  const TextRange& getRange();
- protected:
-  TextRange m_range;
+public:
+	Expression(const TextRange& range);
+	virtual ~Expression();
+	virtual bool isStatement();
+	virtual bool needsSemicolonAfterStatement();
+	virtual const Type& getType();
+	virtual bool isTypeKnown();
+	//returns true if it has a type after the call
+	virtual bool findType() = 0;
+	virtual void printSignature() = 0;
+	const TextRange& getRange();
+protected:
+	TextRange m_range;
 };
 
 
 class VariableExpression : public Expression {
 private:
-  std::string m_name;
+	std::string m_name;
 public:
-  VariableExpression(const std::string& name, const TextRange& range);
-  bool findType();
-  void printSignature();
+	VariableExpression(const std::string& name, const TextRange& range);
+	bool findType();
+	void printSignature();
 };
 
 class IntegerConstantExpression: public Expression {
 private:
-  daf_largest_uint m_integer;
-  NumberLiteralConstants::ConstantIntegerType m_integerType;
+	daf_largest_uint m_integer;
+	NumberLiteralConstants::ConstantIntegerType m_integerType;
 public:
-  IntegerConstantExpression(daf_largest_uint integer, NumberLiteralConstants::ConstantIntegerType integerType, const TextRange& range);
-  void printSignature();
-  bool findType() {return false;}
+	IntegerConstantExpression(daf_largest_uint integer, NumberLiteralConstants::ConstantIntegerType integerType, const TextRange& range);
+	void printSignature();
+	bool findType() {return false;}
 };
 
 class RealConstantExpression : public Expression {
 private:
-  daf_largest_float m_real;
-  NumberLiteralConstants::ConstantRealType m_realType;
+	daf_largest_float m_real;
+	NumberLiteralConstants::ConstantRealType m_realType;
 public:
-  RealConstantExpression(daf_largest_float real, NumberLiteralConstants::ConstantRealType realType, const TextRange& range);
-  void printSignature();
-  bool findType() {return false;}
+	RealConstantExpression(daf_largest_float real, NumberLiteralConstants::ConstantRealType realType, const TextRange& range);
+	void printSignature();
+	bool findType() {return false;}
 };
 
 class ConstantStringExpression : public Expression {
 public:
-  ConstantStringExpression(const std::string& text);
+	ConstantStringExpression(const std::string& text);
 private:
-  std::string m_text;
+	std::string m_text;
 };
 
 class FunctionExpression : public Expression {
 private:
-  FunctionType m_function;
-  std::unique_ptr<Expression> m_body;
+	FunctionType m_function;
+	std::unique_ptr<Expression> m_body;
 public:
-  FunctionExpression(std::vector<FunctionParameter>&& params,
-                     bool isInline, TypeReference&& returnType,
-                     FunctionReturnType returnTypeType,
-                     std::unique_ptr<Expression>&& body,
-                     const TextRange& range);
-  bool findType() {return false;}
-  bool isTypeKnown() {return true;}
-  Type& getType() {return m_function;}
-  void printSignature();
+	FunctionExpression(std::vector<FunctionParameter>&& params,
+					   bool isInline, TypeReference&& returnType,
+					   FunctionReturnType returnTypeType,
+					   std::unique_ptr<Expression>&& body,
+					   const TextRange& range);
+	bool findType() {return false;}
+	bool isTypeKnown() {return true;}
+	Type& getType() {return m_function;}
+	void printSignature();
 };
 
 //TODO: Use m_ prefix for private fields
 class InfixOperatorExpression : public Expression {
 private:
-  unique_ptr<Expression> LHS;
-  const InfixOperator& op;
-  unique_ptr<Expression> RHS;
+	unique_ptr<Expression> LHS;
+	const InfixOperator& op;
+	unique_ptr<Expression> RHS;
 public:
-  InfixOperatorExpression(std::unique_ptr<Expression>&& LHS, const InfixOperator& op,
-                          std::unique_ptr<Expression>&& RHS);
-  bool findType() {return false;}
-  bool isStatement() {return op.statement;}
-  void printSignature();
+	InfixOperatorExpression(std::unique_ptr<Expression>&& LHS, const InfixOperator& op,
+							std::unique_ptr<Expression>&& RHS);
+	bool findType() {return false;}
+	bool isStatement() {return op.statement;}
+	void printSignature();
 };
 
 //TODO: Use m_ prefix for private fields
 class PrefixOperatorExpression : public Expression {
 private:
-  const PrefixOperator& op;
-  unique_ptr<Expression> RHS;
+	const PrefixOperator& op;
+	unique_ptr<Expression> RHS;
 public:
-  PrefixOperatorExpression(const PrefixOperator& op, int opLine, int opCol, std::unique_ptr<Expression>&& RHS);
-  bool findType() {return false;}
-  bool isStatement() {return op.statement;}
-  void printSignature();
+	PrefixOperatorExpression(const PrefixOperator& op, int opLine, int opCol, std::unique_ptr<Expression>&& RHS);
+	bool findType() {return false;}
+	bool isStatement() {return op.statement;}
+	void printSignature();
 };
 
 //TODO: Use m_ prefix for private fields here too
 class PostfixCrementExpression : public Expression {
 private:
-  bool decrement;
-  unique_ptr<Expression> LHS;
+	bool decrement;
+	unique_ptr<Expression> LHS;
 public:
-  PostfixCrementExpression(std::unique_ptr<Expression>&& LHS, bool decrement, int opLine, int opEndCol);
-  bool findType() {return false;}
-  bool isStatement() {return true;}
-  void printSignature();
+	PostfixCrementExpression(std::unique_ptr<Expression>&& LHS, bool decrement, int opLine, int opEndCol);
+	bool findType() {return false;}
+	bool isStatement() {return true;}
+	void printSignature();
 };
 
 class FunctionCallExpression : public Expression {
 private:
-  unique_ptr<Expression> m_function;
-  std::vector<unique_ptr<Expression>> m_params;
+	unique_ptr<Expression> m_function;
+	std::vector<unique_ptr<Expression>> m_params;
 public:
-  FunctionCallExpression(unique_ptr<Expression>&& function, std::vector<unique_ptr<Expression>>&& parameters, int lastLine, int lastCol);
-  bool findType() {return false;}
-  bool isStatement() {return true;}
-  void printSignature();
+	FunctionCallExpression(unique_ptr<Expression>&& function, std::vector<unique_ptr<Expression>>&& parameters, int lastLine, int lastCol);
+	bool findType() {return false;}
+	bool isStatement() {return true;}
+	void printSignature();
 };
--- Compiler/src/parsing/ast/Scope.cpp
+++ Compiler/src/parsing/ast/Scope.cpp
@@ -5,10 +5,10 @@
 
 Scope::Scope(const TextRange& range, std::vector<std::unique_ptr<Statement>>&& statements,
              std::unique_ptr<Expression> outExpression)
-  : Expression(range), m_statements(std::move(statements)), m_outExpression(std::move(outExpression)) {}
+	: Expression(range), m_statements(std::move(statements)), m_outExpression(std::move(outExpression)) {}
 
 bool Scope::isStatement() {
-  return true;
+	return true;
 }
 
 bool Scope::needsSemicolonAfterStatement() {
@@ -16,23 +16,24 @@ bool Scope::needsSemicolonAfterStatement() {
 		logDaf(WARNING) << "Scopes with outputs need trailing semicolons" << std::endl;
 		return true;
 	}
+
 	return false;
 }
 
 bool Scope::findType() {
-  assert(false);
-  return false;
+	assert(false);
+	return false;
 }
 
 void Scope::printSignature() {
-  std::cout << "{" << std::endl;
-  for(auto statement = m_statements.begin(); statement!=m_statements.end(); ++statement) {
-    (*statement)->printSignature();
-  }
-  if(m_outExpression) {
-    m_outExpression->printSignature();
-    std::cout << std::endl;
-  }
-  std::cout << "}";
+	std::cout << "{" << std::endl;
+	for(auto statement = m_statements.begin(); statement!=m_statements.end(); ++statement) {
+		(*statement)->printSignature();
+	}
+	if(m_outExpression) {
+		m_outExpression->printSignature();
+		std::cout << std::endl;
+	}
+	std::cout << "}";
 }
 
