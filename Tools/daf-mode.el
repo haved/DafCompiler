d53561a0900a0a91328659a62c15cd9b4ef113f2
haved <krogstie.havard@gmail.com>
2017-02-02 18:20:11 +0100

    Add extra words to the emacs syntax highlighting

--- Tools/daf-mode.el
+++ Tools/daf-mode.el
@@ -1,21 +1,20 @@
 ;; The mode for the daf programming language
 ;; So far just syntax highlighting
 
-(setq daf-keywords '("pub" "mut" "let" "def" "ctor" "dtor" "typedef" "namedef" "linkfile"))
-(setq daf-control '("if" "else" "with" "as" "class" "trait"))
+(setq daf-keywords '("pub" "prot" "mut" "let" "def" "virt"  "class" "trait" "ctor" "dtor" "typedef" "namedef" "with" "as" "linkfile"))
+(setq daf-control '("if" "else" "for" "while"))
 (setq daf-literals '("null" "this" "true" "false"))
 (setq daf-types '("This" "u8" "i8" "u16" "i16" "u32" "i32" "u64" "i64" "f32" "f64" "char" "usize" "bool"))
 
-(setq daf-keywords-regexp (regexp-opt daf-keywords 'words-include-escapes))
-(setq daf-control-regexp (regexp-opt daf-control 'words-include-escapes))
-(setq daf-literals-regexp (regexp-opt daf-literals 'words-include-escapes))
-(setq daf-types-regexp (regexp-opt daf-types 'words-include-escapes))
+(setq daf-keywords-regexp (regexp-opt daf-keywords 'words))
+(setq daf-control-regexp (regexp-opt daf-control 'words))
+(setq daf-literals-regexp (regexp-opt daf-literals 'words))
+(setq daf-types-regexp (regexp-opt daf-types 'words))
 
 (setq daf-font-lock-keywords
 	  `(
 		(,"//.*$" . font-lock-comment-face)
-		(,"/\*" . font-lock-comment-start-skip)
-		(,"\*/" . font-lock-comment-end-skip)
+		;; TODO: Add multi line comments
 		(,daf-types-regexp . font-lock-type-face)
         (,daf-literals-regexp . font-lock-constant-face)
         (,daf-control-regexp . font-lock-builtin-face)
