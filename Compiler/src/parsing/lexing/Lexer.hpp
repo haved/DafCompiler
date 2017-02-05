b12ea35159b546e059ca1b7a6bab9f13dfdad16a
haved <krogstie.havard@gmail.com>
2017-02-05 22:34:55 +0100

    Re-indent Lexer.hpp (big change coming! :)

--- Compiler/src/parsing/lexing/Lexer.hpp
+++ Compiler/src/parsing/lexing/Lexer.hpp
@@ -10,39 +10,39 @@ using std::string;
 
 class Lexer {
 private:
-  const FileForParsing& fileForParsing;
-  std::ifstream infile;
-  Token tokens[TOKEN_COUNT_AMOUNT];
-  int currentToken;
-  int line;
-  int col;
-  char currentChar;
-  char lookaheadChar;
-  void advanceChar();
-  char parseOneChar();
-  bool parseStringLiteral(Token& token);
-  bool parseCharLiteral(Token& token);
+	const FileForParsing& fileForParsing;
+	std::ifstream infile;
+	Token tokens[TOKEN_COUNT_AMOUNT];
+	int currentToken;
+	int line;
+	int col;
+	char currentChar;
+	char lookaheadChar;
+	void advanceChar();
+	char parseOneChar();
+	bool parseStringLiteral(Token& token);
+	bool parseCharLiteral(Token& token);
 
-  //All related to number constant parsing. A bit ugly, yeah
-  int parseBase(string& text);
-  void eatMainDigits(string& text, int base, bool* real);
-  void checkForExponent(string& text, int base, bool real);
-  void parseNumberLiteralType(string& text, char* type, int* typeSize);
-  void eatRemainingNumberChars();
-  void inferAndCheckFloatType(char* type, int* typeSize, bool realNumber, int line, int col);
-  bool parseNumberLiteral(Token& token);
+	//All related to number constant parsing. A bit ugly, yeah
+	int parseBase(string& text);
+	void eatMainDigits(string& text, int base, bool* real);
+	void checkForExponent(string& text, int base, bool real);
+	void parseNumberLiteralType(string& text, char* type, int* typeSize);
+	void eatRemainingNumberChars();
+	void inferAndCheckFloatType(char* type, int* typeSize, bool realNumber, int line, int col);
+	bool parseNumberLiteral(Token& token);
 public:
-  Lexer(const FileForParsing& file);
-  bool advance();
-  inline Token& getFutureToken(int rel) {return tokens[(currentToken+TOKEN_COUNT_AMOUNT+rel)%TOKEN_COUNT_AMOUNT];}
-  inline Token& getCurrentToken() {return getFutureToken(0);}
-  inline TokenType& currType() {return getCurrentToken().type;}
-  inline Token& getLookahead() {return getFutureToken(1);}
-  inline Token& getSuperLookahead() {return getFutureToken(2);}
+	Lexer(const FileForParsing& file);
+	bool advance();
+	inline Token& getFutureToken(int rel) {return tokens[(currentToken+TOKEN_COUNT_AMOUNT+rel)%TOKEN_COUNT_AMOUNT];}
+	inline Token& getCurrentToken() {return getFutureToken(0);}
+	inline TokenType& currType() {return getCurrentToken().type;}
+	inline Token& getLookahead() {return getFutureToken(1);}
+	inline Token& getSuperLookahead() {return getFutureToken(2);}
 	inline Token& getLastToken() {return getSuperLookahead();}
 	inline Token& getSecondToLastToken() {return getLookahead();}
 	inline Token& getPreviousToken() {return getFutureToken(-1);}
-  inline bool hasCurrentToken() {return currType() != END_TOKEN;}
-  inline const FileForParsing& getFile() {return fileForParsing;}
-  bool expectToken(const TokenType& type);
+	inline bool hasCurrentToken() {return currType() != END_TOKEN;}
+	inline const FileForParsing& getFile() {return fileForParsing;}
+	bool expectToken(const TokenType& type);
 };
