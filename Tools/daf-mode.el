c8e58dcf86a70d4936ee91f28a2f633de1b08d6b
haved <krogstie.havard@gmail.com>
2017-05-05 13:53:44 +0200

    Add Impl as keyword

--- Tools/daf-mode.el
+++ Tools/daf-mode.el
@@ -1,10 +1,10 @@
 ;; The mode for the daf programming language
 ;; So far just syntax highlighting
 
-(setq daf-keywords '("pub" "prot" "let" "def" "mut" "typedef" "namedef" "with" "as" "linkfile" "move" "uncrt" "certain" "class" "trait" "enum" "virt" "ctor" "dtor"))
-(setq daf-control '("if" "else" "for" "while" "break" "continue" "retry" "return"))
+(setq daf-keywords '("pub" "prot" "let" "def" "mut" "typedef" "namedef" "with" "as" "linkfile" "move" "uncrt" "certain" "class" "trait" "enum" "virt" "ctor" "dtor" "#foreign" "#default" "#delete" ""))
+(setq daf-control '("if" "else" "for" "while" "break" "continue" "retry" "return" "instof" "match" "case"))
 (setq daf-literals '("null" "this" "true" "false"))
-(setq daf-types '("This" "u8" "i8" "u16" "i16" "u32" "i32" "u64" "i64" "f32" "f64" "char" "usize" "isize" "bool"))
+(setq daf-types '("This" "Impl" "u8" "i8" "u16" "i16" "u32" "i32" "u64" "i64" "f32" "f64" "char" "usize" "isize" "bool"))
 
 (setq daf-keywords-regexp (regexp-opt daf-keywords 'words))
 (setq daf-control-regexp (regexp-opt daf-control 'words))
@@ -13,8 +13,7 @@
 
 (setq daf-font-lock-keywords
 	  `(
-		(,"//.*$" . font-lock-comment-face)
-		;; TODO: Add multi line comments
+		;(,"//.*$" . font-lock-comment-face)
 		(,daf-types-regexp . font-lock-type-face)
         (,daf-literals-regexp . font-lock-constant-face)
         (,daf-control-regexp . font-lock-builtin-face)
@@ -22,11 +21,13 @@
 				))
 
 ;;;###autoload
-(define-derived-mode daf-mode fundamental-mode "daf mode"
+(define-derived-mode daf-mode c-mode "daf mode"
 	"Major mode for editing daf files"
 
 	;;syntax highlighting
 	(setq font-lock-defaults '((daf-font-lock-keywords))))
+
+(add-to-list 'auto-mode-alist '("\\.daf\\'" . daf-mode))
 
 (setq daf-keywords nil)
 (setq daf-control nil)
