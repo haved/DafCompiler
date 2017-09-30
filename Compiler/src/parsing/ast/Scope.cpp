e5fe2f51782455befe242a4aefef40734e6ef24d
haved <krogstie.havard@gmail.com>
2017-09-30 20:08:50 +0200

    Finish Scope.[ch]pp

--- Compiler/src/parsing/ast/Scope.cpp
+++ Compiler/src/parsing/ast/Scope.cpp
@@ -1,6 +1,7 @@
 #include "parsing/ast/Scope.hpp"
 #include "parsing/lexing/Lexer.hpp"
 #include "DafLogger.hpp"
+#include "parsing/semantic/ConcretableHelp.hpp"
 #include <iostream>
 
 Scope::Scope(const TextRange& range, std::vector<std::unique_ptr<Statement>>&& statements,
@@ -19,22 +20,6 @@ bool Scope::evaluatesToValue() const { //override
 	return !!m_outExpression; //We know outExpression evaluatesToValue
 }
 
-void Scope::makeConcrete(NamespaceStack& ns_stack) {
-	ScopeNamespace scopeNs;
-	ns_stack.push(&scopeNs);
-	for(auto it = m_statements.begin(); it != m_statements.end(); ++it) {
-	    scopeNs.addStatement(**it);
-		(*it)->makeConcrete(ns_stack);
-	}
-	ns_stack.pop();
-}
-
-ConcreteTypeAttempt Scope::tryGetConcreteType(DotOpDependencyList& depList) { //override
-	if(m_outExpression)
-		return m_outExpression->tryGetConcreteType(depList);
-    return ConcreteTypeAttempt::here(getVoidType());
-}
-
 void Scope::printSignature() { //override
 	std::cout << "{" << std::endl;
 	for(auto statement = m_statements.begin(); statement!=m_statements.end(); ++statement) {
@@ -47,6 +32,49 @@ void Scope::printSignature() { //override
 	std::cout << "}";
 }
 
+ConcretableState Scope::makeConcreteInternal(NamespaceStack& ns_stack, DependencyMap& depMap) {
+	ScopeNamespace scopeNs;
+	ns_stack.push(&scopeNs);
+
+	auto concrete = allConcrete();
+	auto lost = anyLost();
+
+	for(auto it = m_statements.begin(); it != m_statements.end(); ++it) {
+	    scopeNs.addStatement(**it);
+		ConcretableState state = (*it)->makeConcrete(ns_stack, depMap);
+	    concrete = concrete << state;
+		lost = lost << state;
+		if(state == ConcretableState::TRY_LATER)
+			depMap.makeFirstDependentOnSecond(this, it->get());
+	}
+
+	if(m_outExpression) {
+		ConcretableState state = m_outExpression->makeConcrete(ns_stack, depMap);
+		concrete = concrete << state;
+		lost = lost << state;
+		if(state == ConcretableState::TRY_LATER)
+			depMap.makeFirstDependentOnSecond(this, m_outExpression.get());
+	}
+
+	ns_stack.pop();
+
+	if(concrete)
+		return retryMakeConcreteInternal(depMap);
+	if(lost)
+		return ConcretableState::LOST_CAUSE;
+	return ConcretableState::TRY_LATER;
+}
+
+ConcretableState Scope::retryMakeConcreteInternal(DependencyMap& depMap) {
+	(void) depMap;
+	
+	if(m_outExpression)
+		m_typeInfo = m_outExpression->getTypeInfo();
+	else
+		m_typeInfo = ExprTypeInfo(getVoidType(), ValueKind::ANONYMOUS);
+
+	return ConcretableState::CONCRETE;
+}
 
 ScopeNamespace::ScopeNamespace() : m_definitionMap() {}
 
--- Compiler/src/parsing/ast/Scope.hpp
+++ Compiler/src/parsing/ast/Scope.hpp
@@ -18,12 +18,11 @@ public:
 	Scope(const TextRange& range, std::vector<std::unique_ptr<Statement>>&& statements, std::unique_ptr<Expression> finalOutExpression);
 	virtual bool isStatement() override; //true
 	virtual bool evaluatesToValue() const override; //We can only be a finalOutExpression if we ourselves have one
-
-	virtual ConcreteTypeAttempt tryGetConcreteType(DotOpDependencyList& depList) override;
-	virtual void makeConcrete(NamespaceStack& ns_stack) override;
-
 	virtual void printSignature() override;
 	inline Expression& getFinalOutExpression() { assert(m_outExpression); return *m_outExpression; }
+
+    virtual ConcretableState makeConcreteInternal(NamespaceStack& ns_stack, DependencyMap& depMap) override;
+	virtual ConcretableState retryMakeConcreteInternal(DependencyMap& depMap) override;
 };
 
 //Used to temporarily know all definitions declared previously in the scope
