5ee70ceca2f96f2ae833f91a90c47706afd3a022
haved <krogstie.havard@gmail.com>
2017-12-23 18:52:34 +0100

    Make buildScript.py, only need to silence some output from dafc and it's good to go

--- buildScript.py
+++ buildScript.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from sys import argv
-import os.path, os, subprocess
+import os.path, os, subprocess, re
+
+printDafCompileOutput = False
 
 binaryName = "buildScript.py"
 def fatal_error(text, *arg):
@@ -34,10 +36,10 @@ knownOptions = {"-buildDir": "DebugBuild",
                 "-tests": False,
                 "-testFolder": "CompilerTests",
                 "-testFilter": r"^\w+\.daf$",
-                "-testDafOutput": "Testing/dafOutput.o",
-                "-testCppFile": "Testing/dafMainCaller.cpp",
-                "-testCppOutput": "Testing/dafMainCaller.o",
-                "-testBinaryOutput": "Testing/outputBinary"}
+                "-testDafOutput": "CompilerTests/Testing/dafOutput.o",
+                "-testCppFile": "CompilerTests/Testing/dafMainCaller.cpp",
+                "-testCppOutput": "CompilerTests/Testing/dafMainCaller.o",
+                "-testBinaryOutput": "CompilerTests/Testing/outputBinary"}
 
 releaseDefaults = {"-buildDir": "ReleaseBuild",
                    "-X86Only": "false",
@@ -112,6 +114,12 @@ def main():
     options.fillInDefaults()
 
     doCMake(options)
+    doMake(options)
+
+    if options.getOption("-tests"):
+        compileDafCaller(options)
+        doTests(options)
+
 
 def tryDo(lamb):
     try:
@@ -132,7 +140,18 @@ def doCMake(options):
     cmakeCommand = ["cmake", cmakeDir]
     cmakeCommand += ["-DDAF_LINK_ALL_LLVM:BOOL=" + options.getOption("-allLLVM")]
     cmakeCommand += ["-DDAF_DEBUG_MACRO:BOOL=" + ("false" if options.getOption("-release") else "true")]
-    cmakeCommand += ["-DDAF_EXTRA_CXX_FLAGS:STRING="+options.getOption("-extraCppFlags")]
+    extraCppFlags = [options.getOption("-extraCppFlags")]
+    O_opt = options.getOption("O")
+    if O_opt != "none":
+        extraCppFlags += ["O"+O_opt]
+    if options.getOption("g") == "true":
+        extraCppFlags += ["-g"]
+    warn_opt = options.getOption("W")
+    if warn_opt in ["all", "pedantic"]:
+        extraCppFlags += ["-Wall", "-Weffc++"]
+        if warn_opt == "pedantic":
+            extraCppFlags += ["-Wextra", "-pedantic"]
+    cmakeCommand += ["-DDAF_EXTRA_CXX_FLAGS:STRING="+" ".join(extraCppFlags)]
     cmakeCommand += ["-DDAF_EXTRA_LINK_FLAGS:STRING="+options.getOption("-extraLinkerFlags")]
     cmakeCommand += options.getOption("-extraCMakeArgs").split()
 
@@ -143,7 +162,104 @@ def doCMake(options):
         if cmakeCall.returncode is not 0:
             fatal_error("Cmake failed with error code {}", cmakeCall.returncode)
 
-    
+def doMake(options):
+    buildDir = options.getOption("-buildDir")
+    if not os.path.exists(buildDir):
+        fatal_error("Missing build dir all of a sudden");
+
+    makeCommand = ["make"]
+    makeCommand += options.getOption("-extraMakeArgs").split()
+
+    info("Running command: {}", "   ".join(makeCommand))
+
+    with subprocess.Popen(makeCommand, cwd=buildDir) as makeCall:
+        makeCall.wait()
+        if makeCall.returncode is not 0:
+            fatal_error("Make failed with error code {}", makeCall.returncode)
+
+
+def compileDafCaller(options):
+    compileCommand = ["g++", "-c", options.getOption("-testCppFile"),
+                      "-o", options.getOption("-testCppOutput"), "-std=c++17", "-Wall", "-Wextra"]
+
+    info("Running command: {}", "   ".join(compileCommand))
+
+    with subprocess.Popen(compileCommand) as compileCall:
+        compileCall.wait()
+        if compileCall.returncode is not 0:
+            fatal_error("Compiling C++ main file failed with error code: {}", compileCall.returncall)
+
+def doTests(options):
+    filesToConsider = []
+
+    topFolder = options.getOption("-testFolder")
+    def pat(file):
+        return os.path.join(topFolder, file)
+
+    def dig(folder, filesToConsider):
+        list = [os.path.join(folder, item) for item in os.listdir(pat(folder))]
+        filesToConsider += [item for item in list if os.path.isfile(pat(item))]
+        for dir in [item for item in list if os.path.isdir(pat(item))]:
+            dig(dir, filesToConsider)
+
+    dig("", filesToConsider)
+
+    filter = re.compile(options.getOption("-testFilter"))
+    filesToConsider = [file for file in filesToConsider if filter.match(file)]
+
+    info("Testing {} files", len(filesToConsider))
+
+    for index, file in enumerate(filesToConsider):
+        testString = "({}/{}): {}".format(index+1, len(filesToConsider), file)
+        info("Testing {}", testString)
+        dafCompileCommand = [os.path.join(options.getOption("-buildDir"),"DafCompiler")]
+        dafCompileCommand += [os.path.join(options.getOption("-testFolder"), file)]
+        dafCompileCommand += ["-o", options.getOption("-testDafOutput")]
+        try:
+            with subprocess.Popen(dafCompileCommand, stdout=subprocess.stdout if printDafCompileOutput else subprocess.PIPE) as dafCompile:
+                dafCompile.wait(timeout=10)
+                if dafCompile.returncode is not 0:
+                    info("last command: {}", "   ".join(dafCompileCommand))
+                    if not printDafCompileOutput:
+                        info("STDOUT for this command: ")
+                        print(dafCompile.stdout.read())
+                    fatal_error("DafCompiler in test {} failed with return code {}", testString, dafCompile.returncode)
+        except TimeoutError as e:
+            info("last command: {}", "   ".join(dafCompileCommand))
+            fatal_error("DafCompiler in test {} timed out", testString)
+
+        linkCommand = ["g++", options.getOption("-testDafOutput"), options.getOption("-testCppOutput")]
+        linkCommand += ["-o", options.getOption("-testBinaryOutput")]
+        try:
+            with subprocess.Popen(linkCommand) as link:
+                link.wait(timeout=10)
+                if link.returncode is not 0:
+                    info("last command: {}", "   ".join(linkCommand))
+                    fatal_error("Linking failed in test {} with return code {}", testString)
+        except TimeoutError as e:
+            info("last command: {}", "   ".join(linkCommand))
+            fatal_error("Linking in test {} timed out", testString)
+
+        runFileCommand = [options.getOption("-testBinaryOutput")]
+        try:
+            with subprocess.Popen(runFileCommand) as run:
+                run.wait(timeout=10)
+                if run.returncode is not 0:
+                    info("last command: {}", "   ".join(runFileCommand))
+                    fatal_error("Running test {} failed with error code {}", testString, run.returncode)
+        except TimeoutError as e:
+            info("last command: {}", "   ".join(linkCommand))
+            fatal_error("Running test {} timed out", testString)
+
+    remove = [options.getOption("-testDafOutput"), options.getOption("-testCppOutput"), options.getOption("-testBinaryOutput")]
+    info("Removing {}, {} and {}", *remove)
+    for file in remove:
+        try:
+            os.remove(file)
+        except FileNotFoundError as e:
+            info("File didn't exist: {}", file)
+
+    info("All {} tests successful", len(filesToConsider))
 
 def printHelpMessage(knownOptions):
     default = [val for key,val in knownOptions.items()]
