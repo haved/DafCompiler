0c6d89bbe6114b53ea759a6205cdaab1b6d22164
haved <krogstie.havard@gmail.com>
2018-02-17 22:01:17 +0100

    Change FunctionSignature.hpp such than FunctionType no longer is a Type, and FunctionExpression has a lot more work to do

--- Compiler/src/parsing/ast/FunctionSignature.hpp
+++ Compiler/src/parsing/ast/FunctionSignature.hpp
@@ -24,62 +24,33 @@ using param_list = std::vector<unique_ptr<FunctionParameter> >;
 class Let;
 using parameter_let_list = std::vector<unique_ptr<Let> >;
 
-class FunctionType : public Type, public ConcreteType, public Namespace {
+class FunctionType {
 private:
 	param_list m_parameters;
 	ReturnKind m_givenReturnKind;
 	optional<TypeReference> m_givenReturnType;
-	optional<FunctionExpression*> m_functionExpression;
 
-	ExprTypeInfo m_returnTypeInfo;
-	optional<ExprTypeInfo> m_implicitCallReturnTypeInfo;
+public:
+	FunctionType(param_list&& parameters, ReturnKind givenReturnKind,
+				 optional<TypeReference> givenReturnType, TextRange& range);
+
+	void printSignature();
+
+	bool addReturnKindModifier(ReturnKind kind);
+};
+
+class FunctionExpression : public Expression, public ConcreteType, public Namespace {
+private:
+	unique_ptr<FunctionType> m_type;
+
+	optional<unique_ptr<Expression>> m_function_body;
+	optional<std::string> m_function_name;
 
 	parameter_let_list m_parameter_lets;
 	NamedDefinitionMap m_parameter_map;
 
-	bool makeConcreteNeverCalled();
-	bool isConcrete();
-public:
-	FunctionType(param_list&& parameters, ReturnKind givenReturnKind,
-				 optional<TypeReference> givenReturnType, TextRange& range);
-	~FunctionType();
-
-	virtual ConcreteType* getConcreteType() override;
-	virtual ConcreteTypeKind getConcreteTypeKind() const override;
-	virtual void printSignature() override;
-
-	bool addReturnKindModifier(ReturnKind kind);
-	void setFunctionExpression(FunctionExpression* body);
-	FunctionExpression* getFunctionExpression();
-	bool hasReturn();
-	bool isReferenceReturn();
-	bool canBeCalledImplicitlyOnce();
-	param_list& getParameters();
-	parameter_let_list& getParameterLetList();
-	virtual Definition* tryGetDefinitionFromName(const std::string& name) override;
-
-	bool readyParameterLets();
-	virtual ConcretableState makeConcreteInternal(NamespaceStack& ns_stack, DependencyMap& depMap) override;
-	virtual ConcretableState retryMakeConcreteInternal(DependencyMap& depMap) override;
-
-	ExprTypeInfo& getReturnTypeInfo();
-	optional<ExprTypeInfo>& getImplicitCallReturnTypeInfo();
-
-	llvm::FunctionType* codegenFunctionType(CodegenLLVM& codegen);
-	virtual bool hasSize() override;
-	virtual llvm::Type* codegenType(CodegenLLVM& codegen) override;
-};
-
-bool isFunctionType(const ExprTypeInfo& typeInfo);
-bool isFunctionType(const ConcreteType* type);
-FunctionType* castToFunctionType(ConcreteType* type);
-
-
-class FunctionExpression : public Expression {
-private:
-	unique_ptr<FunctionType> m_type;
-	optional<unique_ptr<Expression>> m_function_body;
-	optional<std::string> m_function_name;
+	ExprTypeInfo m_returnTypeInfo;
+	optional<ExprTypeInfo> m_implicitCallReturnTypeInfo;
 
 	bool m_broken_prototype, m_filled_prototype;
 	llvm::Function* m_prototype;
@@ -94,15 +65,30 @@ public:
 	~FunctionExpression();
 
 	virtual ExpressionKind getExpressionKind() const override;
+	virtual ConcreteTypeKind getConcreteTypeKind() const override;
 	virtual void printSignature() override;
-
-	FunctionType* getFunctionType();
-	Expression* getBody();
 
 	void setFunctionName(std::string& name);
 
+	bool hasReturn();
+	bool isReferenceReturn();
+	bool canBeCalledImplicitlyOnce();
+	param_list& getParameters();
+	parameter_let_list& getParameterLetList();
+	virtual Definition* tryGetDefinitionFromName(const std::string& name) override;
+
+	bool readyParameterLets();
+	Expression* getBody();
+
+	virtual bool hasSize() override;
+
 	virtual ConcretableState makeConcreteInternal(NamespaceStack& ns_stack, DependencyMap& depMap) override;
 	virtual ConcretableState retryMakeConcreteInternal(DependencyMap& depMap) override;
+
+	ExprTypeInfo& getReturnTypeInfo();
+	optional<ExprTypeInfo>& getImplicitCallReturnTypeInfo();
+
+	virtual llvm::Type* codegenType(CodegenLLVM& codegen) override;
 
 	llvm::Function* tryGetOrMakePrototype(CodegenLLVM& codegen);
 
