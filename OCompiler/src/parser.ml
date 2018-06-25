b2fa7bdfbda50be220595ed5413438a387231634
haved <krogstie.havard@gmail.com>
2018-06-25 15:12:31 +0200

    Fixed stupidity in parse_all_definitions

--- OCompiler/src/parser.ml
+++ OCompiler/src/parser.ml
@@ -15,26 +15,26 @@ and parse_definition = parser
                      | [< ' (Token.Def, loc) >] -> Ast.Minus
                      | [< err=(expected "a definition") >] -> raise err
 
-and parse_all_definitions list = parser
-                               | [< >] -> list
-                               | [< defin = parse_definition; rest = parse_all_definitions >] -> [defin :: rest]
+and parse_all_definitions = parser
+                               | [< defin = parse_definition; rest = parse_all_definitions >] -> defin :: rest
+                               | [< >] -> []
 
 let file_to_definition_list file_name =
   let ic = open_in file_name in
   try
     try
       let stream = Lexer.lex (Stream.of_channel ic) in
-      let defins = parse_all_definitions [] stream in
+      let defins = parse_all_definitions stream in
       close_in ic;
       defins
     with
-    | UnexpectedToken ((token, span), expected) as e ->
+    | UnexpectedToken ((token, span), expected) ->
       Log.log_from_file_interval file_name (Span.interval_of_span span) Log.Fatal_Error
         (Printf.sprintf "expected %s before %s" expected (Token.token_to_string token));
-      raise e
-    | UnexpectedEOF expected as e ->
+      []
+    | UnexpectedEOF expected ->
       Log.log_from_file file_name Log.Fatal_Error (Printf.sprintf "expected %s before EOF" expected);
-      raise e
+      []
   with e ->
     close_in_noerr ic;
     raise e
