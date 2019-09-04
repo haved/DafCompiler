08501654dc309705eb16046ae75616f165793f0a
haved <krogstie.havard@gmail.com>
2019-09-04 11:57:01 +0200

    Restart writig buildScript.py

--- buildScript.py
+++ buildScript.py
@@ -1,136 +1,100 @@
 #!/bin/env python3
 from sys import argv
 
-version = "0.0.1"
-switch_name = "dafc_opam_switch/"
-switch_packages = "oasis"
-default_test_path = "Tests/"
-default_test_filter = ".*\\.daf"
-
 def error(*a, **ka):
-    print("buildScript.py: error: ", end="")
+    print("buildScript.py: error: ",end="")
     print(*a, **ka)
     exit(1)
 
+class Option:
+    def __init__(self, name, desc, callback, *, take_value=None, allow_multiple=False):
+        self.name = name
+        self.desc = desc
+        self.callback = callback
 
-def doBuild(options, global_options):
-    if len(options) != 0:
-        error("Unexpected option for build:", options[0])
+        self.take_value = take_value if take_value != None else name.endswith("=")
+        self.allow_multiple = allow_multiple
+        self.executed = False
 
-    print("Starting build")
+    def tryExecute(self, text):
+        if self.take_value:
+            if not text.starts_with(self.name):
+                return False
+            self.execute(text[len(self.name):])
+        else:
+            if text != self.name:
+                return False
+            self.execute()
 
-def doTest(options, global_options):
-    pass
+    def execute(self, *a):
+        self.callback(*a)
 
-def doInstall(options, global_options):
-    if len(options) != 0: #TODO Install location
-        error("Unexpected option for install:", options[0])
+class Command:
+    def __init__(self, name, desc, options, callback):
+        self.name = name
+        self.desc = desc
+        self.options = options
+        self.callback = callback
 
-def doUninstall(options, global_options):
-    if len(options) != 0:
-        error("Unexpected option for uninstall:", options[0])
+    def execute(self, args):
+        def tryOption(arg):
+            for opt in self.options:
+                if opt.tryExecute(arg):
+                    return
+            error("Unrecognized option for '{}' command: {}".format(self.name, arg))
+        for arg in args:
+            tryOption(arg)
 
-def doUpgradeSwitch(options, global_options):
-    if len(options) != 0:
-        error("Unexpected option for switch_upgrade:", options[0])
+        self.callback()
 
-def doClean(options, global_options):
-    if len(options) != 0:
-        error("Unexpected option for clean:", options[0])
+    def get_help_message(self):
+        desc_lines = self.desc.split('\n')
+        text = "    {:<16}{}\n".format(self.name, desc_lines[0])
+        for line in desc_lines[1:]:
+            text += "{:<20}{}\n".format("", line)
+        return text
 
-def doCleanSwitch(options, global_options):
-    if len(options) != 0:
-        error("Unexpected option for switch_clean:", options[0])
+class ArgParser:
+    def __init__(self):
+        self.commands = []
+        self.commands_map = {}
 
-command_map = {
-    "build": doBuild,
-    "test": doTest,
-    "install": doInstall,
-    "uninstall": doUninstall,
-    "upgrade-switch": doUpgradeSwitch,
-    "clean": doClean,
-    "clean-switch": doCleanSwitch,
-}
+    def add_command(self, command):
+        self.commands.append(command)
+        assert command.name not in self.commands_map
+        self.commands_map[command.name]=command
 
-possible_global_options = ["--keep-switch"]
+    def execute(self, args):
+        if len(args) == 0:
+            error("No command given, see --help")
+        command_name = args[0]
+        if command_name in self.commands_map:
+            self.commands_map[command_name].execute(args[1:])
+        else:
+            error("Unrecognized command: ")
+
+    def get_help_message(self):
+        text = "Commands:\n"
+
+        for cmd in self.commands:
+            text += cmd.get_help_message()
+            text += "\n"
+        return text
 
 def main():
-    args = argv[1:]
+    arg_parser = ArgParser()
 
-    global_options = []
-    commands = []
+    
 
-    if len(args) == 0:
-        printHelp()
+    def print_help_message():
+        print("Usage: ./buildScript.py <command> [options]")
+        print("Set up an environment and build,test or install the dafc binary")
+        print()
+        print(arg_parser.get_help_message(),end='')
+        exit(0)
 
-    for arg in args:
-        if arg.startswith("-"):
-            if arg == "--help":
-                printHelp()
-            elif arg in possible_global_options:
-                global_options.append(arg)
-            else:
-                if len(commands) == 0:
-                    error(arg, "is not a global option, and there is no command specified before it")
-                else:
-                    commands[-1].options.push_back(arg)
-        else:
-            commands.append({"name":arg, "options":[]})
-
-    if len(commands) == 0:
-        print("No commands specified. See --help for info")
-
-    for command in commands:
-        name = command["name"]
-        if name not in command_map:
-            error("Unrecognized command:", name)
-        command_map[name](command["options"], global_options)
-
-
-def printHelp():
-    print("""\
-buildScript.py version {}
-Usage: ./buildScript.py [commands]
-Set up an opam environment and build,test and install the daf compiler
-
-Commands:
-    build            Build the daf compiler binary
-                     This will init opam for this process only,
-                     and make an opam switch named {}
-                     unless such a switch already exists.
-                     In this switch, we install the packages {}
-                     Then oasis is used to make the final binary
-
-    test             Run tests of the compiler
-    [--path=path]    If a path is specified, that file / folder is tested
-                     Default: {}
-    [--filter=rgx]   If a filter is specified, only tests with file names
-                     matching the specified regex will be tested
-                     Default: {}
-
-    install          Install the built binary to /usr/local/bin
-                     Requires build to be called first
-
-    uninstall        Remove files installed by install
-
-    upgrade-switch   Update the opam switch,
-                     and install any missing dependencies
-                     or newer versions of them
-
-    clean            Remove files and folders created during the build process
-
-    clean-switch     Remove the opam switch made for compiling dafc
-
-Options:
-    --keep-switch    Try using the currently set opam switch
-                     instead making/using our own.
-                     If the current switch is missing packages,
-                     try doing upgrade_switch with this option enabled.
-
-    --help           Print this message and exit
-""".format(version, switch_name, switch_packages, default_test_path, default_test_filter),end="")
-    exit(0)
-
+    arg_parser.add_command(Command("--help", "Print this help message", [], print_help_message))
+    arg_parser.execute(argv[1:])
 
 if __name__ == "__main__":
     main()
